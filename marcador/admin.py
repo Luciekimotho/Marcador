f662a39135cfb0acb8bb70394d8e827b45e8e516
Luciekimotho <luciekimotho@gmail.com>
2015-10-02 21:08:09 +0300

    Changes in the admin.py

--- marcador/admin.py
+++ marcador/admin.py
@@ -6,7 +6,7 @@ from .models import Bookmark, Tag
 class BookmarkAdmin(admin.ModelAdmin):
 	list_display = ('url', 'title', 'owner', 'is_public', 'date_updated')
 	list_editable = ('is_public',)
-	list_filter = ('is_public', 'owner_username')
+	list_filter = ('is_public', 'owner__username')
 	search_fields = ['url' 'title', 'description']
 	readonly_fields = ('date_created', 'date_updated')
 
