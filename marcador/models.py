ec6889e58867d60b372dae5935f07175e290bbf1
Luciekimotho <luciekimotho@gmail.com>
2015-10-02 12:55:58 +0300

    Added meta data for the models

--- marcador/models.py
+++ marcador/models.py
@@ -1,8 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
 class tag(models.Model):
 	name = models.CharField(max_length=50, unique=True)
+
+
+	class Meta:
+		verbose_name = 'tag'
+		verbose_name_plural = 'tags'
+		ordering = ['name']
+
 
 class Bookmark(models.Model):
 	url = models.URLField()
@@ -13,3 +21,8 @@ class Bookmark(models.Model):
 	date_updated = models.DateTimeField('date updated')
 	owner = models.ForeignKey(User, verbose_name='owner', related_name='bookmarks')
 	tags = models.ManytoManyField(Tag, blank=True)
+
+	class Meta:
+		verbose_name = 'bookmark'
+		verbose_name_plural = 'bookmarks'
+		ordering = ['-date_created']
