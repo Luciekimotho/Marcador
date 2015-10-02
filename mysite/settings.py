2b69d59378c3dce0e79f24bdd25d71ac39dd5934
Luciekimotho <luciekimotho@gmail.com>
2015-10-02 22:16:21 +0300

    Login logout urls for automatic redirect after login and logout

--- mysite/settings.py
+++ mysite/settings.py
@@ -106,3 +106,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+LOGIN_URL = 'mysite_login'
+LOGOUT_URL = 'mysite_logout'
+LOGIN_REDIRECT_URL = 'marcador_bookmark_list'
