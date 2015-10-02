c73033af1b34395c850abe32116351614293e201
Luciekimotho <luciekimotho@gmail.com>
2015-10-02 12:50:08 +0300

    Added models for the bookmarks and the tags

--- marcador/models.py
+++ marcador/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class tag(models.Model):
+	name = models.CharField(max_length=50, unique=True)
+
+class Bookmark(models.Model):
+	url = models.URLField()
+	title = models.CharField('title', max_length='255')
+	description = models.CharField('description', blank=True)
+	is_public = models.BooleanField('public', default=True)
+	date_created = models.DateTimeField('date created')
+	date_updated = models.DateTimeField('date updated')
+	owner = models.ForeignKey(User, verbose_name='owner', related_name='bookmarks')
+	tags = models.ManytoManyField(Tag, blank=True)
